f23be2dade2425502bd91e7289f0fac69cc80424
richz <richarmiguelrodriguez@gmail.com>
2023-07-07 17:45:43 -0500

    dev:server

---
+++ src/Server.js
@@ -0,0 +1,63 @@
+import axios from "axios"
+//CREATE A CLASS TO GET AVAIBLE ACCESS
+
+
+class ApiConnection{
+  constructor(route){
+    this.url = 'http://localhost:10801/'
+    this.route = route
+  }
+
+
+  async getToken(){
+    const {data} = await axios.post(${this.url}/auth/login,{
+      email:'elesclenderlugo@gmail.com',
+      password:'12345M'
+    })
+    return data.token
+  }
+
+
+
+  async getInstance(){
+    const token = await this.getToken()
+    const api = axios.create({
+      baseURL: this.url,
+      timeout: 1000,
+      headers: {'Authorization': token}
+    });
+
+
+    return api
+  }
+
+  async getAllData(){
+    let instance = await this.getInstance();
+    const { data } = await instance.get(this.route.get)
+
+    return data
+  }
+
+  async createData(body){
+    let instance = await this.getInstance();
+    const { data } = await instance.post(this.route.post,body)
+
+    return data
+  }
+
+  async deleteData(id){
+    let instance = await this.getInstance();
+    const { data } = await instance.delete(${this.route.delete}/${id})
+    return data
+  }
+
+  async updateData(id,body){
+    let instance = await this.getInstance();
+    const { data } = await instance.put(${this.route.put}/${id},body)
+    return data
+  }
+
+
+}
+
+export default ApiConnection

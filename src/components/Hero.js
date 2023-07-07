792a69828f72018f45c4d8f884388f357291fdae
EduardoJair <112762817+EduardoJair@users.noreply.github.com>
2023-07-07 17:23:56 -0500

    dev: Home

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+const Hero = () => {
+    return (
+      <div className="shadow-neon w-100 bg-cover bg-hero-pattern grid place-content-center mb-36" style={{height:500}} >
+        <div className="w-64 h-64 grid place-content-center text-center" >
+          <h1 className="font-bold" style={{fontSize:'50px'}} >Airlne</h1>
+          <p>Obten informacion acerca de vuelos disponibles, hoteles a tus destinos favoritos y mas..</p>
+        </div>
+      </div>
+    )
+  }
+  
+  export default Hero
---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import { Button } from "@material-tailwind/react";
+import { Link } from 'react-router-dom';
+
+import logo from "../assets/Logo.png"
+
+const Navbar = () => {
+
+  const buttons = [
+    'Vuelos',
+    'Hoteles',
+    'Paquetes',
+    'Reservas',
+    'Reclamos',
+    'PasajeroFrecuente',
+    'Categorias',
+    "Pasajeros"
+  ]
+
+  return(
+    <nav class="bg-hero w-100 h-14 flex flex-row " >
+      <img src={logo} class=" w-50 h-10 pt-1.5" alt="" />
+
+      <div className="flex flex-row justify-end pr-3 pt-1.5 gap-4" style={{width:'87%'}}>
+
+        {
+          buttons.map((button, index) => {
+            return (
+              <Button  className='p-2 
+                font-[Roboto Mono] 
+                rounded-2xl bg-transparent font-thin
+                hover:bg-white hover:text-hero'>
+                  <Link to={/${button.toLowerCase()}} >{button}</Link>
+              </Button>
+            )
+          })
+        }
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar

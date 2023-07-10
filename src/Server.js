94063a40f0f1b1ae7c066440ee4f387d43fc6644
Esclender-Lugo <e00181703@certus.edu.pe>
2023-07-10 08:44:54 -0500

    localhost

--- src/Server.js
+++ src/Server.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 class ApiConnection {
   constructor (route) {
-    this.url = 'http://20.102.31.112:10801'
+    this.url = 'http://localhost:10801'
     this.route = route
   }
 
